3b89699f130eb1ba3bf0aab9269692f391bc2634
menip <https://github.com/menip>
2017-07-28 21:29:11 -0700

    Initial commit

---
+++ desktop/src/com/menip/ldjam39/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.menip.ldjam39.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.menip.ldjam39.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
